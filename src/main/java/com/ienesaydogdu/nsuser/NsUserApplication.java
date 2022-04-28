05f279f276c44c612285b49d31cfa97393bb47b0
ienesaydogdu <aydogduenes.i@gmail.com>
2022-04-29 02:51:07 +0300

    initial

---
+++ src/main/java/com/ienesaydogdu/nsuser/NsUserApplication.java
@@ -0,0 +1,13 @@
+package com.ienesaydogdu.nsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ienesaydogdu/nsuser/NsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ienesaydogdu.nsuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
